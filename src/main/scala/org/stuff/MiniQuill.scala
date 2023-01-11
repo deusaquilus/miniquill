1c8eddc4f2f13ed56468becf35dca542d15250dd
Alexander Ioffe <deusaquilus@gmail.com>
2023-01-11 13:39:47 -0500

    One possible dynamic behavior

--- src/main/scala/org/stuff/MiniQuill.scala
+++ src/main/scala/org/stuff/MiniQuill.scala
@@ -17,16 +17,49 @@ object MiniQuill:
   case class FunctionApply(function: Ast, values: List[Ast]) extends Ast
   case class Block(statements: List[Ast]) extends Ast
   case class Val(name: Ident, body: Ast) extends Ast
+  sealed trait Vase extends Ast
+  object Vase:
+    case class Brandished(content: Expr[Ast]) extends Vase
+    case class Sheathed(content: Ast) extends Vase
+
+  def containsVase(ast: Ast): Boolean =
+    ast match
+      case Entity(name) => false
+      case Ident(name) => false
+      case Filter(query, alias, body) => containsVase(query) || containsVase(body)
+      case Map(query, alias, body) => containsVase(query) || containsVase(body)
+      case FlatMap(query, alias, body) => containsVase(query) || containsVase(body)
+      case Property(ast, name) => containsVase(ast)
+      case Tuple(values) => values.exists(containsVase(_))
+      case Constant(value) => false
+      case Function(params, body) => containsVase(ast)
+      case FunctionApply(function, values) => containsVase(function) || values.exists(containsVase(_))
+      case Block(statements) => statements.exists(containsVase(_))
+      case Val(name, body) => containsVase(body)
+      case BinaryOperation(left, op, right) => containsVase(left) || containsVase(right)
+      case Vase.Brandished(content) => true
+      case Vase.Sheathed(content) => true
+
 
   sealed trait Operator
   object Operator:
     case object `==` extends Operator
     case object `&&` extends Operator
+    case object `||` extends Operator
+    case object `+` extends Operator
+    case object `-` extends Operator
+    case object `*` extends Operator
+    case object `/` extends Operator
+
+  sealed trait Liveness
+  object Liveness:
+    case object Static extends Liveness
+    case object Dynamic extends Liveness
 
   case class BinaryOperation(left:Ast, op:Operator, right:Ast) extends Ast
 
-  case class Quoted[T](ast: Ast):
-    def unquote = throw new IllegalArgumentException("Only a compile-time-construct")
+  case class Quoted[T](ast: Ast, liveness: Liveness):
+    def unquote: T = throw new IllegalArgumentException("Only a compile-time-construct")
 
   class Query[T] {
     def filter(e:T => Boolean): Query[T] 	    = throw new IllegalArgumentException("This can only be used inside a quoted block")
@@ -58,14 +91,29 @@ object MiniQuill:
         def astParse(expr: Expr[Any]): Ast =
           expr match
             case '{ ($q: Quoted[t]).unquote } => astParse(q)
-            case '{ Quoted.apply[t]($ast) } => Unlifter(ast)
+
+            case '{ Quoted.apply[t]($astExpr, $livenessExpr) } =>
+              Unlifter.liveness(livenessExpr) match
+                case Liveness.Static =>
+                  Unlifter(astExpr)
+                case Liveness.Dynamic =>
+                  report.warning("Using a quotation that has Dynamic Liveness. Whole AST be wrapped into a vase.")
+                  Vase.Brandished(astExpr)
+
             case '{ query[t] } => Entity(TypeRepr.of[t].classSymbol.get.name)
             case '{ ($query: Query[t]).filter(${Lambda1(alias, body)}) } => Filter(astParse(query), Ident(alias), astParse(body))
             case '{ ($query: Query[t]).withFilter(${Lambda1(alias, body)}) } => Filter(astParse(query), Ident(alias), astParse(body))
             case '{ ($query: Query[t]).map[mt](${Lambda1(alias, body)}) } => Map(astParse(query), Ident(alias), astParse(body))
             case '{ ($query: Query[t]).flatMap[mt](${Lambda1( alias, body)}) } => FlatMap(astParse(query), Ident(alias), astParse(body))
+
             case NamedOp1(left, "==", right) => BinaryOperation(astParse(left), Operator.==, astParse(right))
             case NamedOp1(left, "&&", right) => BinaryOperation(astParse(left), Operator.&&, astParse(right))
+            case NamedOp1(left, "||", right) => BinaryOperation(astParse(left), Operator.||, astParse(right))
+            case NamedOp1(left, "+", right) => BinaryOperation(astParse(left), Operator.+, astParse(right))
+            case NamedOp1(left, "-", right) => BinaryOperation(astParse(left), Operator.-, astParse(right))
+            case NamedOp1(left, "*", right) => BinaryOperation(astParse(left), Operator.*, astParse(right))
+            case NamedOp1(left, "/", right) => BinaryOperation(astParse(left), Operator./, astParse(right))
+
             case Unseal(Apply(TypeApply(Select(TupleIdent(), "apply"), types), values)) => Tuple(values.map(v => astParse(v.asExpr)))
             case block @ Unseal(TBlock(parts, lastPart)) if (parts.length > 0) =>
               val partsAsts =
@@ -80,8 +128,54 @@ object MiniQuill:
 
             case id @ Unseal(i @ TIdent(x)) =>
               val owner = Symbol.spliceOwner
-              if (isTermExternal(i)) report.warning(s"The term: ${i.show} is external to the quote")
-              Ident(x)
+
+              enum IdentType:
+                case Raw
+                case Quoted
+                case Dynamic
+
+              val constantable =
+                i.tpe.asType match
+                  case '[Int] => IdentType.Raw
+                  case '[Long] => IdentType.Raw
+                  case '[Short] => IdentType.Raw
+                  case '[Double] => IdentType.Raw
+                  case '[Float] => IdentType.Raw
+                  case '[Boolean] => IdentType.Raw
+                  case '[Byte] => IdentType.Raw
+                  case '[String] => IdentType.Raw
+                  case '[Quoted[Int]] => IdentType.Quoted
+                  case '[Quoted[Long]] => IdentType.Quoted
+                  case '[Quoted[Short]] => IdentType.Quoted
+                  case '[Quoted[Double]] => IdentType.Quoted
+                  case '[Quoted[Float]] => IdentType.Quoted
+                  case '[Quoted[Boolean]] => IdentType.Quoted
+                  case '[Quoted[Byte]] => IdentType.Quoted
+                  case '[Quoted[String]] => IdentType.Quoted
+                  case _ => IdentType.Dynamic
+
+              if (isTermExternal(i))
+                constantable match
+                  case IdentType.Raw =>
+                    println(s"The term: ${i.show} is external to the quote")
+                    Vase.Brandished('{ Constant(${i.asExpr}) })
+                  case IdentType.Quoted =>
+                    println(s"The quotation: ${i.show} is external to the quote")
+                    val quotedExpr = i.asExprOf[Quoted[_]]
+                    Vase.Brandished('{ ${quotedExpr}.ast })
+                  case IdentType.Dynamic =>
+                    report.throwError(s"Cannot vase a type of: ${Printer.TypeReprShortCode.show(i.tpe.widen)}. Must be a constant type or quotation of a constant type.")
+              else
+                Ident(x)
+
+            case Unseal(Literal(IntConstant(i))) => Constant(i)
+            case Unseal(Literal(LongConstant(i))) => Constant(i)
+            case Unseal(Literal(ShortConstant(i))) => Constant(i)
+            case Unseal(Literal(DoubleConstant(i))) => Constant(i)
+            case Unseal(Literal(FloatConstant(i))) => Constant(i)
+            case Unseal(Literal(BooleanConstant(i))) => Constant(i)
+            //case Unseal(Literal(ByteConstant(i))) => Constant(i)
+            case Unseal(Literal(StringConstant(i))) => Constant(i)
 
             case Unseal(Typed(inside /*Term*/, _)) => astParse(inside.asExpr)
             case _ => report.throwError(
@@ -95,8 +189,17 @@ object MiniQuill:
       end Parser
 
       val quillAst: Ast = Parser.astParse(quotedRaw)
+      val liveness =
+        if (containsVase(quillAst))
+          report.warning(s"Vase detected in the AST. Liveness will be dynamic. (${quillAst})")
+          Liveness.Dynamic
+        else
+          report.info(s"Ast is Static: (${quillAst})")
+          Liveness.Static
+
       val liftedQuillAst: Expr[Ast] = Lifter(quillAst)
-      '{ Quoted($liftedQuillAst) }
+      val livenessAst: Expr[Liveness] = Lifter.liveness(liveness)
+      '{ Quoted($liftedQuillAst, $livenessAst) }
     }
   }
 
@@ -104,6 +207,7 @@ object MiniQuill:
   /** =========================== Unlift ======================== **/
   object Unlifter:
     def apply(ast: Expr[Ast]): Quotes ?=> Ast = unliftAst.apply(ast) // can also do ast.lift but this makes some error messages simpler
+    def liveness(value: Expr[Liveness]): Quotes ?=> Liveness = unliftLiveness.apply(value)
 
     extension [T](t: Expr[T])(using FromExpr[T], Quotes)
       def unexpr: T = t.valueOrError
@@ -114,6 +218,11 @@ object MiniQuill:
         import quotes.reflect._
         unlift.lift(expr).getOrElse { throw new IllegalArgumentException(s"Could not Unlift ${Printer.TreeShortCode.show(expr.asTerm)}") }
       def unapply(expr: Expr[T])(using Quotes): Option[T] = unlift.lift(expr)
+
+    given unliftLiveness: NiceUnliftable[Liveness] with
+      def unlift =
+        case '{ Liveness.Static } => Liveness.Static
+        case '{ Liveness.Dynamic } => Liveness.Dynamic
 
     given unliftProperty: NiceUnliftable[Property] with
       def unlift =
@@ -145,11 +254,17 @@ object MiniQuill:
       def unlift =
         case '{ Operator.== } =>  Operator.==
         case '{ Operator.&& } =>  Operator.&&
+        case '{ Operator.|| } =>  Operator.||
+        case '{ Operator.+ } =>  Operator.+
+        case '{ Operator.- } =>  Operator.-
+        case '{ Operator.* } =>  Operator.*
+        case '{ Operator./ } =>  Operator./
   end Unlifter
 
   /** =========================== Lift ======================== **/
   object Lifter:
     def apply(ast: Ast): Quotes ?=> Expr[Ast] = liftableAst(ast)
+    def liveness(value: Liveness): Quotes ?=> Expr[Liveness] = liftableLiveness(value)
 
     extension [T](t: T)(using ToExpr[T], Quotes)
       def expr: Expr[T] = Expr(t)
@@ -170,6 +285,11 @@ object MiniQuill:
     extension [T: Type](list: List[T])(using ToExpr[T], Quotes)
       def spliceVarargs = Varargs(list.map(Expr(_)).toSeq)
 
+    given liftableLiveness : NiceLiftable[Liveness] with
+      def lift =
+        case Liveness.Static => '{ Liveness.Static }
+        case Liveness.Dynamic => '{ Liveness.Dynamic }
+
     given liftableEntity : NiceLiftable[Entity] with
       def lift =
         case Entity(name: String) => '{ Entity(${name.expr})  }
@@ -177,6 +297,12 @@ object MiniQuill:
     given liftableTuple: NiceLiftable[Tuple] with
       def lift =
         case Tuple(values) => '{ Tuple(${values.expr}) }
+
+    given liftableVase: NiceLiftable[Vase] with
+      def lift =
+        case Vase.Brandished(content) => '{ Vase.Sheathed($content) }
+        // technically should not run into this case because a sheathed vase should not ever be lifted
+        case Vase.Sheathed(content) => '{ Vase.Sheathed(${content.expr}) }
 
     given liftableAst : NiceLiftable[Ast] with
       def lift =
@@ -196,12 +322,18 @@ object MiniQuill:
           '{ BinaryOperation(${a.expr}, ${castOp}, ${b.expr})  }
         case v: Property => liftableProperty(v)
         case v: Ident => liftableIdent(v)
+        case v: Vase => liftableVase(v)
 
     import Operator.{ == => ee}
     given liftOperator : NiceLiftable[Operator] with
       def lift =
         case _: ee.type => '{ Operator.== }
         case Operator.&& => '{ Operator.&& }
+        case Operator.|| => '{ Operator.|| }
+        case Operator.+ => '{ Operator.+ }
+        case Operator.- => '{ Operator.- }
+        case Operator.* => '{ Operator.* }
+        case Operator./ => '{ Operator./ }
   end Lifter
 
 end MiniQuill
--- src/main/scala/org/stuff/UseMiniQuill.scala
+++ src/main/scala/org/stuff/UseMiniQuill.scala
@@ -10,17 +10,25 @@ object UseMiniQuill {
   def main(args: Array[String]):Unit = {
 
     val i = 123
+    inline def iInline = 123
 
-    inline def q = quote(
-      for {
-        ii <- query[Int]
-        p <- query[Person]
-        a <- query[Address] if (p.id == i)
-      } yield (p.firstName, a.street)
+    val iQuoted = quote(123)
+    println(s"iQuoted: ${iQuoted}")
+
+    // val q = quote(
+    //   for {
+    //     //ii <- query[Int]
+    //     p <- query[Person]
+    //     a <- query[Address] if (p.id == iQuoted.unquote)
+    //   } yield (p.firstName, a.street)
+    // )
+
+    val q = quote(
+        query[Int].map(v => v + iQuoted.unquote)
     )
 
-    // //
 
-    println(q)
+
+    println(q) // //
   }
 }
